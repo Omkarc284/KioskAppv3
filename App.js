58148d9417ed484816ec938ade19d6ddb19b248b
Omkar Chauhan <77777642+Omkarc284@users.noreply.github.com>
2023-01-13 19:41:41 +0530

    Woking expo setup

--- App.js
+++ App.js
@@ -11,9 +11,11 @@ import RatingScreen from "./src/screens/Review";
 import { Provider as AuthProvider } from './src/context/AuthContext';
 import { setNavigator } from "./src/navigationRef";
 import ResolveAuthScreen from "./src/screens/ResolveAuthScreen";
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect, useState } from "react";
 // const [kioskName, setKioskName] = useState('');
 // const [isLoggedIn, setIsLoggedIn] = useState(false);
-
+SplashScreen.preventAutoHideAsync();
 const navigator = createSwitchNavigator(
   {
     ResolveAuth : ResolveAuthScreen,
@@ -40,6 +42,24 @@ const navigator = createSwitchNavigator(
 const App = createAppContainer(navigator);
 
 export default () => {
+  const [appIsReady, setAppIsReady] = useState(false);
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+        await SplashScreen.hideAsync();
+      }
+    }
+
+    prepare();
+  }, []);
   return (
     <AuthProvider>
       <App ref={(navigator) => {setNavigator(navigator)}} />
